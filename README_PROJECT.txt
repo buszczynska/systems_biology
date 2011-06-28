46264b6d20fb95a1c58304d08bd42fe856255282
zuz <callisto.zuz@gmail.com>
2011-06-28 17:27:34 +0200

    adding scripts

---
+++ README_PROJECT.txt
@@ -0,0 +1,136 @@
+1. Opis bazy danych
+
+Plik bioinfodb.sql zawiera stworzona przez nas baze danych. Zawiera ona 8 przykladowych artykulow,
+poniewaz wieksza ilosc bardzo spowalnia przeszukiwanie bazy. Najpierw nalezy otworzyc mysql i stworzyc
+pusta baze danych:
+mysql> create database bioinfo;
+Nastepnie zamykamy mysql i w terminalu wpisac polecenie:
+> mysql --user=name_of_user --password bioinfo < bioinfodb.sql
+Nastepnie wchodzimy do mysql i definiujemy baze danych, na ktorej bedziemy pracowac:
+mysql> use bioinfo; 
+
+a) Baza danych sklada sie z 3 tabel: paragrahps, words, wcount (poszczegolne tabele
+zastepuja osobne bazy danych). Zeby wyswietlic tabelki nalezy uzyc polecenia mysql:
+mysql> show tables; 
++-------------------+
+| Tables_in_bioinfo |
++-------------------+
+| paragraphs        |
+| wcount            |
+| words             |
++-------------------+
+3 rows in set (0.00 sec)
+
+b) Tabela words zawiera informacje na temat paragrafow (PMID_LIST), w ktorych wystepuje dane slowo(WORD)
+oraz liczbe ogolna liczbe wystapien tego slowa w calej bazie danych:
+mysql> show columns from words;
++-----------------+-------------+------+-----+---------+-------+
+| Field           | Type        | Null | Key | Default | Extra |
++-----------------+-------------+------+-----+---------+-------+
+| WORD            | varchar(50) | NO   | PRI | NULL    |       |
+| PMID_LIST       | longtext    | NO   |     | NULL    |       |
+| NRALLAPPEARANCE | bigint(20)  | NO   |     | NULL    |       |
++-----------------+-------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+
+c) Tabela paragrahps zawiera informacje na temat listy slow (WORD_LIST) w danym paragrafie kazdego artykulu (PMID_PNR, gdzie PMID
+nr artykulu, a PNR to numer paragrafu):
+mysql> show columns from paragraphs;
++-----------+-------------+------+-----+---------+-------+
+| Field     | Type        | Null | Key | Default | Extra |
++-----------+-------------+------+-----+---------+-------+
+| PMID_PNR  | varchar(20) | NO   | PRI | NULL    |       |
+| WORD_LIST | longtext    | NO   |     | NULL    |       |
++-----------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
+d) Tabela wcount zawiera informacje na temat czestosci (NRPARAGAPPEARANCE) wystepowania danego slowa (WORD)
+w konkretnym paragrafie konkretnego artykulu (PMID_PNR):
+mysql> show columns from wcount;
++-------------------+-------------+------+-----+---------+-------+
+| Field             | Type        | Null | Key | Default | Extra |
++-------------------+-------------+------+-----+---------+-------+
+| PMID_PNR          | varchar(20) | NO   | PRI | NULL    |       |
+| WORD              | varchar(50) | NO   | PRI | NULL    |       |
+| NRPARAGAPPEARANCE | bigint(20)  | NO   |     | NULL    |       |
++-------------------+-------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+ 
+2. Opis programu xml_parser6c.py
+Program jest parserem do publikacji w formacie xml. Opis poszczegolnych funkcji znajduje sie w programie
+bezposrednio pod linia ich definicji.
+
+Opis dzialania algorytmu:
+- z pliku xml pobiera te czesci artykulu, ktore nas interesuja, np. istotny jest tytul i body artykulu,
+a nieistotne sa np. references;
+- interesujace nas czesci (wybrane w poprzednim punkcie) dzieli na paragrafy;
+- nastepnie z kazdego paragrafu usuwa stopwords (stopwords2.txt), znaki interpunkcyjne z konca i poczatku
+stringa oraz dziwne znaki zaczynajace sie na &#x, ktore wystepuja osobno, na koniec pozostale slowa
+zamienia na ich podstawy slowotworcze.
+- algorytm zwraca 3-elementowa krotke ('pmid', 'nr paragrafu', 'lista slow wystepujacych w danym paragrafie'),
+na przyklad: 
+('16103898', 1, ['ab', 'initio', 'predict', 'transcript', 'factor', 'target', 'structur', 'knowledg'])
+
+Parser ten wykorzystywany jest przy robieniu insertow do bazy danych, ale moze byc uzywany,
+jako niezalezny program.
+Nalezy wtedy odkomentowac fragment, znajdujacy sie na koncu pliku xml_parser6c.py:
+
+if __name__ == '__main__':
+    xml = XmlParser()
+    xml_doc = open('nazwa_pliku_zawierajacego_artykul_w_formacie_xml.nxml','r').read()
+    bs_doc = bs(xml_doc)
+    xml_doc_important = xml.get_important_parts(xml.important_parts_list, bs_doc)
+    paragraphs_list = xml.get_paragraphs(xml_doc_important)
+    cleaned_tuples = xml.remove_junk(xml.get_text_from_paragraphs(xml_doc_important, bs_doc))
+    #returns one list of touples from one .nxml file
+    print cleaned_tuples[0]
+
+i uruchomic program w terminalu:
+> python xml_parser6c.py
+
+3. Opis programu insertdb.py
+W celu ulatwienia wprowadzania artykulow do bazy danych stworzylismy skrypt insertdb.py.
+Opis poszczegolnych funkcji znajduje sie w programie bezposrednio pod linia ich definicji.
+
+Opis dzialania algorytmu:
+- program parsuje wszystkie pliki w formacie .nxml znajdujace sie w roboczym katalogu,
+korzystajac ze skryptu xml_parser6c.py
+- w ten sposob uzyskane dane zostaja wprowadzone do bazy
+
+Przed uruchomieniem programu nalezy zdefiniowac uzytkownika bazy danych i sama baze danych
+zmieniajac linijke w if __name__ == '__main__':
+#DB connection
+db = my.connect(host='localhost', user='root', passwd='bioinfo5', db='bioinfodb')
+
+Nastepnie uruchamiammy program w terminalu:
+> python insertdb.py
+
+4. Opis programu statisticdb.py
+Program zawiera funkcje wykonujace przykladowe zapytania do bazy danych oraz obliczajace z-score
+2 slow. Opis poszczegolnych funkcji znajduje sie w programie bezposrednio pod linia ich definicji.
+
+5. Opis programu run_statisticdb.py
+Program umozliwia uzytkownikowi polaczenie sie z baza danych, pobieranie z niej 
+informacji dotyczacych interesujacego go slowa oraz obliczenie z-score.
+Wynik zapytan  kierowanych do bazy danych oraz obliczona wartosc z-score, zapisywane sa w pliku:
+"Statistics_res.txt"
+
+Uruchomienie programu:
+> python run_statisticdb.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ statisticdb.py
@@ -0,0 +1,129 @@
+#!usr/bin python
+#-*- coding: utf-8 -*-
+
+__doc__ =  "Funkcje umozliwiajace pobieranie informacji z bazy danych, ich wyswietlanei uzytkownikowi, oraz liczenie z-score."
+__author__ = "Bioinformatyka V, UAM, Poznań"
+
+import MySQLdb as my 
+import Stemmer, sys
+import math 
+
+def stem(word):
+    '''Function that returns the 'core' of the given word.'''
+    
+    stemmer = Stemmer.Stemmer('english')
+    word_after = stemmer.stemWord(word)
+    return word_after
+
+# Przykladowe zapytania do bazy danych
+def select_words(word, db_cursor):
+    '''Function that selects the paragraphs with word-query.'''
+    db_cursor.execute('SELECT PMID_LIST, NRALLAPPEARANCE FROM words WHERE WORD = %s', (word))
+    return db_cursor.fetchall()
+    
+def select_paragraphs(nr_par, db_cursor):
+    '''Function that selects the paragraphs with nr_par-query.'''
+    db_cursor.execute('SELECT WORD_LIST FROM paragraphs WHERE PMID_PNR = %s', (nr_par))
+    return db_cursor.fetchall()
+    
+def select_wcount(each_word, nr_par, db_cursor):
+    '''Function that selects the paragraphs with nr_par-query.'''
+    db_cursor.execute('SELECT NRPARAGAPPEARANCE FROM wcount WHERE WORD = %s AND PMID_PNR = %s', (each_word, nr_par))
+    return db_cursor.fetchall()
+    
+def countall(db_cursor):
+    db_cursor.execute('SELECT COUNT(*) FROM paragraphs')
+    return db_cursor.fetchone()   
+
+def zscore(word, correlated_word, db_cursor, file):
+    '''
+    Z-score pozwala obliczyc korelacje pomiedzy dwoma slowami, korzystajac ze wzoru:
+    z-score = ( ( a - ( b * c / d) ) / ( pierwiastek_z ( c * ( ( d - c ) / ( d * d ) * b) ) ) )
+    gdzie:
+    b - liczba paragrafow, w ktorych wystapilo slowo z zapytania (word) - paragrafy te bedziemy okreslac,
+    jako analizowane paragrafy 
+    a - liczba paragrafow, w ktorych wystapilo skorelowane slowo (correlated_word), paragrafy te zostaly
+    wybrane TYLKO z puli analizowanych paragrafow. Czyli wyszukujemy tych paragrafow, w ktorych wystapilo zarowno
+    wyszukiwane, jak i skorelowane slowo.
+    c - liczba paragrafow, w ktorych wystapilo skorelowane slowo (correlated_word), ale wybrana z puli WSZYSTKICH
+    paragrafow znajdujacych sie w bazie danych
+    d - ogolna liczba wszystkich paragrafow znajdujacych sie w bazie danych 
+    '''
+    
+    word_occu = select_words(word, db_cursor)
+    correl_word_occu = select_words(correlated_word, db_cursor)
+    
+    print word, word_occu
+    print correl_word_occu
+    if word_occu  and correl_word_occu :
+	word_occu = word_occu[0][0].split(',')
+	correl_word_occu = correl_word_occu[0][0].split(',')
+	#common_par_num - liczba paragrafow, w ktorych wystepuje zarowno slowo szukane, jak i skorelowane.
+	common_par_num = 0
+    
+	for pmid in correl_word_occu:
+	    if pmid in word_occu:
+		common_par_num +=1
+	
+	print common_par_num
+	if common_par_num == 0:
+	    return 'Brak korelacji!'
+	
+	a = float(common_par_num)
+	b = float(len(word_occu))
+	c = float(len(correl_word_occu))
+	d = float(countall(db_cursor)[0])
+	
+	zscore = (a - ( b * c / d)) / math.sqrt( c * ( ( d - c ) / (d * d)  * b) )
+	#return correlated_num
+	file.write('Z-score dla dwoch slow: ' + word + ' ' + correlated_word + ' wynosi: ' + zscore + '\n')
+	return zscore
+    else:
+	return 'z-score nie zostal policzony poniewaz szukane slowo nie wystepuje w bazie danych'
+    
+def show_words_from_table_words(word, db_cursor, wanted_word, file):
+    # result is a list of touples - [(szukane słowo, lista paragrafów, liczba wystąpień słowa we wszystkich artykułach)]
+    result1 = select_words(word, db_cursor)
+    list_paragraphs = ''
+    numb_all_appear = 0 
+    if len(result1) == 0:
+	print "Nie odnaleziono szukanego slowa w bazie danych!"
+    else:
+	list_paragraphs = ''
+	numb_all_appear = 0 
+	list_paragraphs = result1[0][0]
+	numb_all_appear = result1[0][1]
+
+	print 'Szukane słowo to: ', wanted_word,'\n\nWystępuje ono w następujących paragrafach: ', list_paragraphs, '\n\nWe wszystkich artykułach wystepuje ', numb_all_appear, ' razy/raz.\n\n'
+	tekst = 'Poszukiwane słowo to: ' + wanted_word +'\n\nWystępuje w następujących paragrafach: ' + list_paragraphs + '\n\nWe wszystkich artykułach wystepuje ' + str(numb_all_appear) + ' razy/raz.\n\n'
+	file.write(tekst)
+
+	return list_paragraphs
+
+def show_words_from_paragraphs(word, list_p, cut_v, db_cursor, wanted_word, file):
+  '''Function that returns the statistics of words from paragraphs in which word-query appears.'''
+  if list_p == None:
+      print "Lista paragrafow jest pusta! Dalsza statystyka nie została przeprowadzona!"
+  else:
+      list_paragraphs_div = list_p.split(',')
+      #split the pmid_list record into single pmid_pnr 
+      for nr_par in list_paragraphs_div:
+	  #select from db word_list for given pmid_pnr
+	  result2 = select_paragraphs(nr_par, db_cursor)
+	  print '\n\nW paragrafie ', nr_par, ' znajdują się następujące słowa: ', result2[0][0], '\n\n','Wartosc graniczna dla przeprowadzanych statystyk to: ',cut_v,'\nSlowa, ktorych liczba wystapien jest wieksza niz ta wartosc nie beda brane pod uwage w statystykach. \nSlowa z tego paragrafu i liczba ich wystapien:'
+	  tekst2 = '\n\nW paragrafie ' + nr_par + ' znajdują się następujące słowa: ' + result2[0][0] + '\n\n' + 'Wartosc graniczna dla przeprowadzanych statystyk to: ' + str(cut_v) + '\nSlowa, ktorych liczba wystapien jest wieksza niz ta wartosc nie beda brane pod uwage w statystykach.\nSlowa z tego paragrafu i liczba ich wystapien:\n'
+	  file.write(tekst2)
+	  #split the word_list to make the words statistics
+	  wordlist_div = result2[0][0].split(',')
+        
+	  for each_word in wordlist_div:
+	      try:
+		  #get the nr of word appearance in a given paragraph
+		  result3 = select_wcount(each_word, nr_par, db_cursor)
+		  #If nr of appearance is bigger then cut_v display the word and #
+		  if int(result3[0][0]) > cut_v:
+		      tekst3 = '\n> ' + each_word + ' -- ' + str(result3[0][0]) + ' razy.'
+		      file.write(tekst3)
+		      print '> ', each_word, ' -- ', result3[0][0], ' razy.'
+		  else: pass
+	      except: pass
